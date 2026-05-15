66f78ea77a912f1c5502fe253b52f6558b5252f0
ukjent7 <162427564+ukjent7@users.noreply.github.com>
2026-05-16 07:42:38 +0800

    Update 强制noto sans.js

--- 强制noto sans.js
+++ 强制noto sans.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Force Noto Sans SC
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      3.0
 // @description  强制所有网站使用 Noto Sans SC 字体
 // @author       -
 // @match        https://czbooks.net/*
@@ -10,12 +10,12 @@
 // ==/UserScript==
 
 GM_addStyle(`
-    body, p, div, span, h1, h2, h3, h4, h5, h6, a, li, td, th, input, textarea, button {
+    body, p, div, span, h1, h2, h3, h4, h5, h6, a, li, td, th, input, textarea {
         font-family: "Noto Sans SC", "PingFang SC", "Microsoft YaHei", sans-serif !important;
     }
     code, pre, kbd, samp,
+    .fas, .far, .fal, .fab, .fad, [class*="fa-"],
     .icon, [class*="icon"], [class*="Icon"],
-    [class*="fa-"], [class*=" fa"], .fas, .far, .fal, .fab, .fad,
     i[class], svg text {
         font-family: revert !important;
     }
